146544faf93ce27cff1bb32952b26308018225e9
alielhishi <ali.elheshi@gmail.com>
2024-08-06 14:38:01 +0200

    oneTable

--- backend/models.go
+++ backend/models.go
@@ -1,42 +1,63 @@
 package backend
 
 type ActionChain struct {
-	ID      string   `json:"id"`
-	Webhook Trigger  `json:"webhook"`
-	Actions []Action `json:"actions"`
-}
-
-type HTTPDetails struct {
-	URL     string            `json:"url"`
-	Method  string            `json:"method"`
-	Headers map[string]string `json:"headers"`
-	Body    string            `json:"body,omitempty"`
+	ID       string    `json:"id"`
+	Triggers []Trigger `json:"triggers"`
 }
 
 type Trigger struct {
-	Type string       `json:"type"`
-	HTTP *HTTPDetails `json:"http,omitempty"`
+	Type            string            `json:"type"`
+	URL             string            `json:"url"`
+	Method          string            `json:"method"`
+	Headers         map[string]string `json:"headers"`
+	Body            string            `json:"body"`
+	FollowingAction Action            `json:"followingAction"`
 	// Add other trigger types as needed
 }
 
 type Action interface {
 	GetType() string
-	Execute() error
+	Exec() error
+	GetFollowingAction() Action
 }
 
 type HTTPAction struct {
-	Type string      `json:"type"`
-	HTTP HTTPDetails `json:"http"`
+	URL             string            `json:"url"`
+	Method          string            `json:"method"`
+	Headers         map[string]string `json:"headers"`
+	Body            string            `json:"body"`
+	FollowingAction Action            `json:"followingAction"`
 }
 
-// Implement the Action interface
 func (a HTTPAction) GetType() string {
+	return "http"
+}
+
+func (a HTTPAction) Exec() error {
+	// Implementation here
+	return nil
+}
+
+func (a HTTPAction) GetFollowingAction() Action {
+	return a.FollowingAction
+}
+
+type CoreAction struct {
+	Type            string `json:"type"`
+	FollowingAction Action `json:"followingAction"`
+}
+
+func (a CoreAction) GetType() string {
 	return a.Type
 }
 
-func (a HTTPAction) Execute() error {
+func (a CoreAction) Exec() error {
 	// Implementation here
 	return nil
+}
+
+func (a CoreAction) GetFollowingAction() Action {
+	return a.FollowingAction
 }
 
 // Add other action types as needed
