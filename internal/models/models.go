8fc6a37d5ca0946344d0696c30f68cadc48df3b1
alielhishi <ali.elheshi@gmail.com>
2024-08-13 14:14:58 +0200

    test

--- internal/models/models.go
+++ internal/models/models.go
@@ -287,6 +287,54 @@ func (b *BranchAction) GetFollowingActionID() string {
 	return b.FollowingActionID
 }
 
+type LoopAction struct {
+	ID                string `json:"id"`
+	Description       string `json:"description"`
+	Action            Action `json:"action"`
+	Condition         string `json:"condition"`
+	FollowingActionID string `json:"following_action_id"`
+}
+
+func (l *LoopAction) GetID() string {
+	return l.ID
+}
+
+func (l *LoopAction) GetDescription() string {
+	return l.Description
+}
+
+func (l *LoopAction) GetType() string {
+	return "loop"
+}
+
+func (l *LoopAction) Exec(ctx *Context) error {
+	for {
+		// Execute the action
+		if err := l.Action.Exec(ctx); err != nil {
+			return fmt.Errorf("error executing action: %v", err)
+		}
+
+		// Evaluate the condition
+		conditionMet, err := evaluateCondition(l.Condition, ctx)
+		if err != nil {
+			return fmt.Errorf("error evaluating condition: %v", err)
+		}
+		if conditionMet {
+			break
+		}
+	}
+
+	return nil
+}
+
+func (l *LoopAction) GetResultID() string {
+	return ""
+}
+
+func (l *LoopAction) GetFollowingActionID() string {
+	return l.FollowingActionID
+}
+
 func evaluateCondition(condition string, ctx *Context) (bool, error) {
 	// Split the condition into parts
 	parts := strings.Split(condition, " ")
